12d5a1879b0c40007f093309c9e9ff61b745869c
Shalini396 <85531588+Shalini396@users.noreply.github.com>
2023-06-09 08:27:17 +0530

    Add files via upload

---
+++ Nain.java
@@ -0,0 +1,24 @@
+public class Nain {             
+  public static void main(String[] args) {
+    Person person1 = new Person("Ean Craig", 11);
+    Person person2 = new Person("Evan Ross", 12);
+    System.out.println(person1.getName() + " is " + person1.getAge() + " years old.");
+    System.out.println(person2.getName() + " is " + person2.getAge() + " years old.\n");
+  }
+}
+class Person {
+    private String name;
+    private int age;
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+	//Creation of methods
+    public String getName() {
+        return name;              
+    }
+    public int getAge() {
+        return age;
+    }
+}
+
